f415875ff1c3154fb5220d3bac2c3a928e4f05d5
ChinmayPalaye <palayechinmay@gmail.com>
2021-02-24 17:58:00 +0530

    interface of blockchain

--- src/blockChain.js
+++ src/blockChain.js
@@ -4,24 +4,68 @@ const difficulty = 3;
 
 let validator = require("./validator");
 
-let mongoose = require("mongoose");
+// let mongoose = require("mongoose");
 
 //get the model of the block to save
-let blockChainModel = mongoose.model("vote_blocks");
+// let blockChainModel = mongoose.model("vote_blocks");
+let blockChainModel = require('./database/block-model')
+let NodeModel = require('./database/networkNode-model')
 
 
 let chalk = require("chalk");
 
 class BlockChain {
 
-    constructor(existingChain = []) {
+    constructor(initialChain=[], initialNetworkNodes=[]) {
 
         //Set the chain to be in it's previous state
-        this.chain = existingChain;
-        
+        // this.chain = this.getInitialChain();
+        this.chain = initialChain;
         //Buffer to store incoming votes
         this.curr_votes = [];
+        this.nodeURL = process.argv[3];
+        this.networkNodes = initialNetworkNodes;
+        
+        if(this.chain.length===0) {
+            this.createGenesisBlock();
+        }
+    }
 
+    // getInitialChain = async ()=>{
+    //     let initChain = await blockChainModel.find({index:1});
+    //     return initChain;
+    // }
+    createGenesisBlock() {
+        let block = {
+            index: 1,
+            timestamp: Date.now(),
+            votes: [],
+            prevHash: 0,
+            nonce: 0,
+            hash: 'Genesis Block'
+        };
+        this.saveBlock(block);
+    }
+
+    addNewNode(nodeURL) {
+        let node = (nodeURL.url?nodeURL:{url:nodeURL})
+        // let node = {
+            // url: nodeURL
+        // };
+        if(this.networkNodes.indexOf(node)===-1 && this.nodeURL!==node.url)
+        {
+            this.networkNodes.push(node);
+            let newNode = new NodeModel(node);
+            newNode.save((err)=>{
+                if(err){
+                    return console.log(chalk.red("Cannot add node: "+err.message));
+                console.log(chalk.green('Node added successfully'));
+                }
+            });
+            return true;
+        } else {
+            return false;
+        }
     }
 
     //Function takes the hash of previous block, creates a block with the votes in the buffer,
@@ -38,7 +82,13 @@ class BlockChain {
         if (validator.proofOfWork(block).toString().substring(0,difficulty) === Array(difficulty+1).join('0')) {
             block.hash = hash(block);
             //Add it to the instance Save it on the DB Console Success
-            let newBlock = new blockChainModel(block);
+            this.saveBlock(block);
+            return block;
+        }
+    }
+
+    saveBlock(block) {
+        let newBlock = new blockChainModel(block);
             newBlock.save((err) => {
                 if (err)
                     return console.log(chalk.red("Cannot save Block to DB ", err.message));
@@ -50,14 +100,14 @@ class BlockChain {
                 .push(block);
             this.curr_votes = [];
             return block;
-        }
     }
 
     //add new vote to the buffer
     addNewVote(candidateIn,partyIn) {
         this
             .curr_votes
-            .push({candidate:candidateIn , party: partyIn });
+            .push({candidate: candidateIn, party: partyIn});
+            return {candidate: candidateIn, party: partyIn};
     }
 
     //get the latest block from chain
