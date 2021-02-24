99e78791f23d44b66ae88b97fe25bc6d0a7e7847
ChinmayPalaye <palayechinmay@gmail.com>
2021-02-24 17:58:39 +0530

    APIs for different operations on blockchain

---
+++ src/api/blockchain_api.js
@@ -0,0 +1,156 @@
+const express = require('express')
+const app=express()
+let database = require('./../database')
+let blockChainModel = require('../database/block-model')
+let NodeModel = require('../database/networkNode-model')
+const reqPromise = require('request-promise');
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+
+const BlockChain = require('./../blockChain')
+
+database.onConnect(async ()=>{
+    let blockChain = new BlockChain(await blockChainModel.find({}),await NodeModel.find({}));
+    let port = process.argv[2];
+    // console.log("Chain : ", blockChain.chain);
+
+    app.get('/chain',(req,res)=>{
+        return res.send(blockChain);
+    });
+
+    app.post('/add-vote',(req,res)=>{
+        blockChain.addNewVote(req.body.candidate, req.body.party);
+        return res.json('you voted for '+req.body.candidate+', '+req.body.party);
+        // res.json('Vote will be registered.')
+    });
+
+    app.post('/add-vote/broadcast',(req,res)=>{
+        const vote = blockChain.addNewVote(req.body.candidate, req.body.party);
+        // res.json('you voted for '+req.body.candidate+', '+req.body.party);
+
+        const registerNodes = [];
+        blockChain.networkNodes.forEach(networkNode => {
+            const requestOptions = {
+                uri: networkNode.url + '/add-vote',
+                method: 'POST',
+                body: vote,
+                json: true
+            }
+            registerNodes.push(reqPromise(requestOptions));
+        });
+    
+        Promise.all(registerNodes).then((data)=>{
+            return res.json({message: "vote registered and broadcasted."});
+        });
+    });
+
+    app.post('/add-block',(req,res)=>{
+        const blockIn = req.body.block;
+        const lastBlock = blockChain.lastBlock();
+        if(/*lastBlock==null || */(blockIn.prevHash===lastBlock.hash /*&& lastBlock.index+1 === blockIn.index*/))
+        {
+            blockChain.saveBlock(blockIn);
+            return res.json(
+                {
+                    message: 'Add new Block successfully!',
+                    newBlock: blockIn
+                }
+            );
+        } else {
+            return res.json(
+                {
+                    message: 'Cannot add new Block!',
+                    newBlock: blockIn
+                }
+            );
+        }
+    });
+
+    app.get('/mine',(req,res)=>{
+        let addedBlock = blockChain.addNewBlock(blockChain.lastBlock().hash);
+        // res.json(addedBlock);
+    
+        const requests = [];
+        blockChain.networkNodes.forEach(networkNode => {
+            const requestOptions = {
+                uri: networkNode.url + '/add-block',
+                method: 'POST',
+                body: {block : addedBlock},
+                json: true
+            }
+            requests.push(reqPromise(requestOptions));
+        });
+    
+        Promise.all(requests).then(data => {
+            return res.json(
+                {
+                    message: 'Mining & broadcasting new Block successfully',
+                    newBlock: addedBlock
+                }
+            );
+            
+        }).catch(err=>{
+            console.log('error in broadcast: '+err);
+        });
+    });
+
+    app.post('/register-node',(req,res)=>{
+        if(blockChain.addNewNode(req.body.nodeURL))
+        {
+            return res.json('Node registered successfully');
+        } else {
+            return res.json('Cannot be registered because already present');
+        }
+    });
+
+    app.post('/register-bulk-nodes',(req,res)=>{
+        req.body.networkNodes.forEach((nodeURL)=>{
+            blockChain.addNewNode(nodeURL);
+        });
+        return res.json('Nodes registered successfully');
+    });
+
+    app.post('/register-and-broadcast-node', (req, res)=>{
+        const nodeURL = req.body.nodeURL;
+    
+        blockChain.addNewNode(nodeURL);
+    
+        const registerNodes = [];
+        blockChain.networkNodes.forEach(networkNode => {
+            const requestOptions = {
+                uri: networkNode.url + '/register-node',
+                method: 'POST',
+                body: { nodeURL: nodeURL },
+                json: true
+            }
+            registerNodes.push(reqPromise(requestOptions));
+        });
+    
+        Promise.all(registerNodes)
+            .then(data => {
+                const bulkRegisterOptions = {
+                    uri: nodeURL + '/register-bulk-nodes',
+                    method: 'POST',
+                    body: { networkNodes: [...blockChain.networkNodes, blockChain.nodeURL] },
+                    json: true
+                }
+    
+                return reqPromise(bulkRegisterOptions);
+            }).then(data => {
+                return res.json(
+                    {
+                        message: 'A node registered with network successfully'
+                    }
+                );
+            });
+    });
+
+    app.get('/nodes', (req,res)=>{
+        return res.json({nodeURL: blockChain.nodeURL, networkNodes: blockChain.networkNodes});
+    });
+
+    app.listen(port, ()=>{
+        console.log('Running on port '+port)
+    })
+})
