b68af21f38df196bf2c5e9983b43439e850d9123
ChinmayPalaye <palayechinmay@gmail.com>
2021-02-27 21:12:33 +0530

    Schema to store users

--- src/database/user.js
+++ src/database/user.js
@@ -15,6 +15,11 @@ const UserSchema = new Schema({
     password: {
         type: String,
         required: true
+    },
+    voted: {
+        type: Boolean,
+        required: true,
+        default: false
     }
 });
 
