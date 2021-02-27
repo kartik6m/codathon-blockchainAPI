b12ae203a57ac9eb73777dac132fa5bf115e1d9d
ChinmayPalaye <palayechinmay@gmail.com>
2021-02-27 22:22:05 +0530

    Hash function

--- src/createHash.js
+++ src/createHash.js
@@ -1,5 +1,6 @@
 const SHA256 = require("crypto-js/sha256");
 
+//Hash function used for blockchain operations.
 hash = (data) => {
     hashInput='';
     hashInput = data.index.toString() + data.timestamp.toString() + data.prevHash.toString() + data.nonce.toString() + JSON.stringify(data.votes);
