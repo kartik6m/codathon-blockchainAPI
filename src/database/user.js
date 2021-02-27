89b880c19b704fc9a8103166bde4d20bfada263f
ChinmayPalaye <palayechinmay@gmail.com>
2021-02-27 22:24:25 +0530

    Schema for storing users

--- src/database/user.js
+++ src/database/user.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-// Create Schema
+// Create Schema for the user data
 const UserSchema = new Schema({
     email: {
         type: String,
