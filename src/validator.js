e6c2184babb2415cbd7d02a58e4e6143415a80d9
ChinmayPalaye <palayechinmay@gmail.com>
2021-02-27 22:24:53 +0530

    Functions for proof of work

--- src/validator.js
+++ src/validator.js
@@ -5,13 +5,11 @@ const difficulty = 3;
 //check if the block satisfies the hash puzzle
 module.exports.validProof = (block) => {
     let guessHash = hash(block);
-    // console.log("Hashing: ", guessHash);
     return guessHash.toString().substring(0,difficulty) === Array(difficulty+1).join('0')
 }
 
-//Calculate the proof of work for the block. (mine the block)
+//Calculate the proof of work for the block.
 module.exports.proofOfWork = (block) => {
-    // let proof = 0;
     while (true) {
         if (!module.exports.validProof(block)) {
             block.nonce++;
